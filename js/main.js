7d8217c3133d8f2fd14e251ea0090800c656a392
nada3ssam98 <nadaessam054@gmail.com>
2023-12-24 01:53:46 +0200

    third commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var siteNameInput = document.getElementById("siteName");
 var siteUrlInput = document.getElementById("siteUrl");
+var siteNamealertMessage = document.getElementById("alertNameMessage");
+var siteUrlalertMessage = document.getElementById("alertUrlMessage");
 var arrayList=[]
 if(localStorage.getItem("Bookmark") != null){
     
@@ -10,13 +12,24 @@ if(localStorage.getItem("Bookmark") != null){
 
 //addProduct function
 function addBookmark(){
-    bookmark = {
-        name : siteNameInput.value,
-        url : siteUrlInput.value,
+
+    if( validationName()==true && validationUrl()==true ){
+
+        bookmark = {
+            name : siteNameInput.value,
+            url : siteUrlInput.value,
+        }
+        arrayList.push(bookmark);
+
+        localStorage.setItem("Bookmark" , JSON.stringify(arrayList));
+
+        displayData();
+
+        clearForm();
+
+        siteNameInput.classList.remove("is-valid");
+        siteUrlInput.classList.remove("is-valid");
     }
-    arrayList.push(bookmark);
-    localStorage.setItem("Bookmark" , JSON.stringify(arrayList));
-    displayData();
 }
 
 
@@ -49,4 +62,65 @@ function deleteButton(index){
     arrayList.splice(index , 1);
     localStorage.setItem("Bookmark" , JSON.stringify(arrayList))
     displayData()
+}
+
+function clearForm(){
+    siteNameInput.value="";
+    siteUrlInput.value="";
+}
+
+function validationName(){
+
+    var regex = siteNameInput.value;
+    var regexForName = /^[a-zA-Z]{3,}[0-9]?$/;
+    //case true
+    if( regexForName.test( regex )  ){
+
+        siteNameInput.classList.add( "is-valid" );
+
+        siteNameInput.classList.remove( "is-invalid" );
+
+        siteNamealertMessage.classList.add("d-none")
+
+        return true
+    }
+
+    else{ //not valid
+        siteNameInput.classList.add( "is-invalid" );
+
+        siteNameInput.classList.remove( "is-valid" );
+
+        siteNamealertMessage.classList.remove("d-none");
+
+        return false
+
+    }
+}
+
+function validationUrl(){
+
+    var regex = siteUrlInput.value;
+    var regexForUrl = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/;
+    //case valid
+    if( regexForUrl.test( regex ) ){
+
+        siteUrlInput.classList.add( "is-valid" );
+
+        siteUrlInput.classList.remove( "is-invalid" );
+
+        siteUrlalertMessage.classList.add("d-none")
+
+        return true
+    }
+
+    else{ //not valid
+        siteUrlInput.classList.add( "is-invalid" );
+
+        siteUrlInput.classList.remove( "is-valid" );
+
+        siteUrlalertMessage.classList.remove("d-none");
+
+        return false
+
+    }
 }
